0cc61e3834bec25501d97d74e79fab38c887e046
Alvin Ang <alvinangbs@gmail.com>
2013-10-07 17:47:11 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Summary
+
+This folder stores all files related to Michael Hartl's [Ruby on Rails Tutorial](http://ruby.railstutorial.org/ruby-on-rails-tutorial-book).
+This tutorial is created using rails 4.0.0 and ruby 2.0.0p267.
+
+Questions? Feel free to contact me at via github.
+
+Created on: October 7th, 2013
---
+++ demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '9516297b2d104c92f49879db3f3efa3a08e1bb0e8fd38b524725f20c9c68fd24eb1d3d97c7760ac132f5213ea601a2c50d2f2d64aae093d59420b5b06ac9d4c1'

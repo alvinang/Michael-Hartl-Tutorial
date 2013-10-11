609828ce9f61094cf360879a7d3b202e8edbe61d
Alvin Ang <alvinangbs@gmail.com>
2013-10-10 19:23:37 -0500

    complete chapter 6

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -2,10 +2,9 @@ class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
 
   validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i  # indicating a constant
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+\.[a-z]+\z/i  # indicating a constant
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }      # rails still infers that uniqueness is true
-
-
-  #has_secure_password
+  has_secure_password
+  validates :password, length: { minimum: 6 }
 end
